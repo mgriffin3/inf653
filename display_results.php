96783769603e1ec87d45c5784b0adf567f897149
mgriffin3 <31641846+mgriffin3@users.noreply.github.com>
2020-02-05 00:18:00 -0500

    Add files via upload

---
+++ display_results.php
@@ -0,0 +1,26 @@
+<?php
+    // get the data from the form
+    $firstname = $_GET['first_name'];
+    $lastname = $_GET['last_name'];
+    $age = $_POST['age'];
+    
+    //set message
+    $message = 'Hello, my name is ' . $firstname . ' ' . $lastname . '. I am ' . $age . ' years old and I am ';
+    if ($age < 18){
+        $message .= 'not ';
+    }
+    $message .= 'old enough to vote in the United States.';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <main>     
+        <span><?php echo $message; ?></span><br>
+
+    </main>
+</body>
+</html>
