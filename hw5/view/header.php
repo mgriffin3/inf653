23e57f6c915f4379e5662abded0498a8d0765d60
mgriffin3 <31641846+mgriffin3@users.noreply.github.com>
2020-02-27 00:53:34 -0500

    Add files via upload

---
+++ hw5/view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>My To Do List</title>
+    <link rel="stylesheet" type="text/css"
+          href="/css/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My To Do List</h1></header>
